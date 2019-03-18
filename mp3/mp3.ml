f24697a5d4fbbd8c9b973b25b0779a4a4337aa8b
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-03-18 01:37:40 -0400

    added mp3

---
+++ mp3/mp3.ml
@@ -0,0 +1,19 @@
+(* File: mp3.ml *)
+
+open Mp3common
+
+(* Problem 1 *)
+let rec import_list lst = raise (Failure "Not implemented yet.")
+
+(* Problem 2 *)
+let pair_sums = LetInExp ("x", VarExp "x", VarExp "x") (* This is wrong! Change it. *)
+
+(* Problem 3 *)
+let rec count_const_in_exp exp =  raise (Failure "Not implemented yet.")
+
+(* Problem 4 *)
+let rec freeVarsInExp exp = raise (Failure "Not implemented yet")
+
+(* Problem 5 *)
+let rec cps_exp e k =  raise (Failure "Not implemented yet.")
+
